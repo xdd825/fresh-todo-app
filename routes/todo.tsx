80bc6d8e2876274767aedd628f4ac06da7497a21
Ben Force <xdd741852963@gmail.com>
2022-07-03 11:48:30 +0800

    first commit

---
+++ routes/todo.tsx
@@ -0,0 +1,12 @@
+/** @jsx h */
+import {h} from "preact";
+import {tw} from "@twind";
+import TodoComponent from "../islands/TodoComponent.tsx";
+
+export default function Todo() {
+    return (
+        <div class={tw`w-screen h-screen flex flex-col justify-center items-center`}>
+            <TodoComponent />
+        </div>
+    )
+}
